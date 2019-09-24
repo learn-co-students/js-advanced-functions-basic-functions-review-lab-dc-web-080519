0fab6d59866e44764b3ef7375f1a2a10b3695784
p6doyle <p6doyle@gmail.com>
2019-09-24 10:47:55 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Your code here
+function saturdayFun(activity="roller-skate"){
+  return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity="go to the office"){
+  return `This Monday, I will ${activity}.`
+}
+
+let wrapAdjective = function(style="*"){
+  return function(adjective="special") {
+    return `You are ${style}${adjective}${style}!`
+  }
+}
+
+let Calculator = {
+  add: function(){
+    return (1 + 3)
+  },
+  subtract: function(){
+    return (1 - 3)
+  },
+  multiply: function(){
+    return (1 * 3)
+  },
+  divide: function(){
+    return (10 / 5)
+  },
+}
+
+let actionApplyer = function(start, ray) {
+  let a = start
+
+  for (let i = 0; i < ray.length; i++ ){
+    a = ray[i](a)
+  }
+
+  return a
+}
